3198d8c984b1247952f4fbdcb4a5719bd268ba7b
Rudy Andres Rivera Aviles <RUDY.ANDRES.RIVERA@GMAIL.COM>
2024-06-19 01:06:04 -0600

    Fixed Paymethod bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable implements JWTSubject
         'address',
         'city',
         'zipcode',
-        'payment',
+        'paymethod',
         'disabled'
     ];
 
